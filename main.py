f41a18084ddfdbb601d052a935bb2d7c14a727a3
Andrey Shishkov <shishkoffandrey@yandex.ru>
2024-12-02 18:24:32 +0300

    Add upload_file function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from contextlib import asynccontextmanager
 from aiobotocore.session import get_session
 
@@ -21,3 +22,14 @@ class S3Client:
     async def get_client(self):
         async with self.session.create_client('s3', **self.config) as client:
             yield client
+
+    async def upload_file(self,
+                          file_path: str,
+                          ):
+        object_name = os.path.basename(file_path)
+        async with self.get_client() as client:
+            with open(file_path, 'rb') as file:
+                await client.put_object(
+                    Bucket=self.bucket_name,
+                    Body=file,
+                )
